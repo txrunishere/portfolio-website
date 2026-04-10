5e68da0a2aa2f4cdfe3dd5df90d4078e113d70a9
txrunishere <tsoni9286@gmail.com>
2026-04-10 11:02:09 +0530

    feat: implement ProjectCard component and update Projects to use it; remove GitHubCalendar

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,12 @@ import { Introduction } from "@/components/introduction";
 import { Projects } from "@/components/projects";
 import { Skills } from "@/components/skills";
 import { BlurFade } from "@/components/ui/blur-fade";
-import { GitHubCalendar } from "react-github-calendar";
 
 export default function Home() {
   return (
     <div className="mx-auto w-full max-w-2xl flex-1 px-4 py-10 sm:py-20">
       <BlurFade className="flex flex-col gap-6">
         <Introduction />
-
-        <div>
-          <GitHubCalendar username="txrunishere" colorScheme="dark" />
-        </div>
 
         <Skills />
 
